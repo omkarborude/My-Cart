e5c06c661bbd7de9d7fa514bd773415df6f9a606
omkar <borudeomkar41@gmail.com>
2021-03-14 22:02:12 +0530

    Feat

---
+++ my-cart/src/Products.js
@@ -0,0 +1,75 @@
+export const Products = [
+    {
+      id: 1,
+      img:"./productsimages/iphone-12.jpg",
+      name: "iPhone 12",
+      price: 89999,
+      rating: "* * * * *",
+      off: "12%"
+    },
+    {
+      id: 2,
+      img:"./productsimages/iphone-8.jpg",
+      name: "iPhone 8",
+      price: 51999,
+      rating: "* * * * ",
+      off: "25%"
+    },
+    {
+      id: 3,
+      img:"./productsimages/s21-plus.jpg",
+      name: "Samsung S21 +",
+      price: 69999,
+      rating: "* * * * *",
+      off: "26%"
+    },
+    {
+      id: 4,
+      img: "./productsimages/note-10.jpg",
+      name: "Samsung Note 10",
+      price: 49000,
+      rating: "* * * * *",
+      off: "35%"
+    },
+    {
+      id: 5,
+      img: "./productsimages/iphone-x.jpg",
+      name: "iPhone X",
+      price: 39000,
+      rating: " * * * *",
+      off: "45%"
+    },
+    {
+      id: 6,
+      img: "./productsimages/s21ultra.jpg",
+      name: "Samsung S21 Ultra",
+      price: 99000,
+      rating: " * * * * *",
+      off: "11%"
+    },
+    {
+      id: 7,
+      img: "./productsimages/iphone-12pro.jpg",
+      name: "iPhone 12 Pro",
+      price: 79000,
+      rating: " * ** * *",
+      off: "18%"
+    },
+    {
+      id: 8,
+      img: "./productsimages/samsung-fold.jpg",
+      name: "Samsung Fold",
+      price: 120000,
+      rating: " * * * * *",
+      off: "8%"
+    },
+    {
+      id: 9,
+      img: "./productsimages/oneplus-nord.jpg",
+      name: "OnePlus Nord",
+      price: 26000,
+      rating: " * * * * *",
+      off: "16%"
+    }
+  ];
+  
